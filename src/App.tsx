538a94dff5021210b8839b2020df1c937e0bd5e8
wellbeck190 <wellbeck190@yahoo.com>
2021-11-23 16:27:37 +0700

    hello

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header"> 
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
